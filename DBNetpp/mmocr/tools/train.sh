510daee8d40878a89daa0d2ee74b98cfb4d28db9
hoho303 <hoangthuan2092003@gmail.com>
2023-10-29 08:06:01 +0700

    push dbnetpp

---
+++ DBNetpp/mmocr/tools/train.sh
@@ -0,0 +1,3 @@
+python3 train.py /mlcv/WorkingSpace/Personals/ngocnd/BKAI2023/BKAI-NAVER-OCR-2023/Models/OCR/mmocr/configs/textdet/dbnetpp/dbnetpp_r50dcnv2_fpnc_1200e_naver2023.py \
+                --work-dir /mlcv/WorkingSpace/Personals/ngocnd/BKAI2023/BKAI-NAVER-OCR-2023/Models/OCR/mmocr/workdir/dbnet \
+                --no-validate 
