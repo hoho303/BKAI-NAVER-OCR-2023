cdcfb9f29f4625f30c0b9245254f8d2426e78797
hoho303 <hoangthuan2092003@gmail.com>
2023-10-29 07:56:56 +0700

    push train sh

---
+++ scripts/train_mmocr_060.sh
@@ -0,0 +1,3 @@
+cd /workspace/WordArt
+python3 tools/train.py /workspace/WordArt/configs/textrecog/corner_transformer/corner_transformer_academic.py \
+        --work-dir workdir/corner_transformer 
